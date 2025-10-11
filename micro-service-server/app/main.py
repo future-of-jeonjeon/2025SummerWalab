bf4b5982a8cab366d3d897d15445e3a95987e7a7
Whal3 <leehan416@gmail.com>
2025-10-12 02:52:07 +0900

    refactor: change startup routine

--- micro-service-server/app/main.py
+++ micro-service-server/app/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI
-from app.utils.logging import logger
-from app.config.settings import settings
-from app.security.cors import setup_cors
+import asyncio
+from contextlib import asynccontextmanager
 from datetime import datetime
+from fastapi import FastAPI
 from sqlalchemy.orm import configure_mappers
-from app.config.database import engine, Base
 from app.auth import routes as auth_routes
-from app.problem import routes as problem_routes
-from app.workbook import routes as workbook_routes
+from app.code_autosave.listener import code_save_listener  # 리스너 import
+from app.config.database import engine, Base
+from app.config.settings import settings
 from app.execution import routes as execution_routes
+from app.problem import routes as problem_routes
+from app.security.cors import setup_cors
+from app.utils.logging import logger
+from app.workbook import routes as workbook_routes
 
 app = FastAPI(**settings.fastapi_kwargs)
 setup_cors(app)
@@ -19,8 +22,12 @@ app.include_router(workbook_routes.router)
 app.include_router(execution_routes.router)
 
 
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def start_up():
+    #redis pubsub listener
+    asyncio.create_task(code_save_listener())
+
+    # db set
     try:
         configure_mappers()
         async with engine.begin() as conn:
@@ -28,6 +35,7 @@ async def startup_event():
         logger.info("DB init success")
     except Exception:
         logger.exception("DB init fail")
+
 
 
 def root():
